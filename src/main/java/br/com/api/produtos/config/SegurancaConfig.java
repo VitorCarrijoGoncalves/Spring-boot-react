00492f2005655cc24d3a4349f51a39f724f1866e
vitor.goncalves <vitor.goncalves@oabgo.org.br>
2023-04-05 22:04:49 -0300

    Implementações iniciais para o uso do token na aplicação

---
+++ src/main/java/br/com/api/produtos/config/SegurancaConfig.java
@@ -0,0 +1,19 @@
+package br.com.api.produtos.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@EnableWebSecurity
+@EnableAuthorizationServer
+@EnableResourceServer
+public class SegurancaConfig extends WebSecurityConfigurerAdapter {  
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("alexandre").password("123").roles("ADMIN");
+    }
+    
+}
