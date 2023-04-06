0273f7c7ce90ad0e21293080e8df2f8d5b57e478
vitor.goncalves <vitor.goncalves@oabgo.org.br>
2023-04-05 22:14:29 -0300

    Implementação final para o consumo do token por uma aplicação externa

--- src/main/java/br/com/api/produtos/config/SegurancaConfig.java
+++ src/main/java/br/com/api/produtos/config/SegurancaConfig.java
@@ -1,8 +1,12 @@
 package br.com.api.produtos.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
@@ -11,9 +15,20 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableResourceServer
 public class SegurancaConfig extends WebSecurityConfigurerAdapter {  
 
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().withUser("alexandre").password("123").roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
     
 }
